253b0b9e488c17708c52c2e6631a4790fabb5d1f
Maxud <m.daudov@corp.mail.ru>
2021-12-24 15:30:32 +0300

    added brotli

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package gb23
 import (
 	"fmt"
 
+	"github.com/andybalholm/brotli"
 	"github.com/valyala/fasthttp"
 )
 
 func main() {
 	fmt.Println(fasthttp.StateActive)
+	fmt.Println(brotli.BestCompression)
 }
